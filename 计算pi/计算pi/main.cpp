ddb90bcc3ff473d939bba16b502db21f75f1fb37
five_high <z393533493@163.com>
2018-07-03 17:33:21 +0800

    添加pi计算的注释

--- 计算pi/计算pi/main.cpp
+++ 计算pi/计算pi/main.cpp
@@ -10,6 +10,12 @@
 
 using namespace std;
 
+// π的计算公式如下：
+// π = 16 * arctan(1 / 5.0) - 4 * arctan(1 / 239);
+//l  其中arctan用如下形式的级数计算：
+// arctan(x) = x - x**3 / 3 + x**5 / 5 ....
+//l  直到级数某项绝对值不大于10-15为止；π和x均为double型。
+
 //实现arctan函数
 double arctan(double x) {
     double sqr = x * x;
