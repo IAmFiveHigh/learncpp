0db05ed1067401daf3f8b2c77b2abcb500b108df
five_high <z393533493@163.com>
2018-07-25 19:11:02 +0800

    单目运算符重载

--- 运算符重载/运算符重载/main.cpp
+++ 运算符重载/运算符重载/main.cpp
@@ -50,6 +50,64 @@ public:
     }
 };
 
+class Clock {
+private:
+    int min;
+    int hour;
+    int sec;
+    
+public:
+    Clock(int newhour=0, int newmin=0, int newsec=0) {
+        if (newhour < 24) {
+            hour = newhour;
+        }else {
+            hour = newhour % 24;
+        }
+        
+        if (newmin < 60) {
+            min = newmin;
+        }else {
+            min = newmin % 60;
+            hour += 1;
+        }
+        
+        if (newsec < 60) {
+            sec = newsec;
+        }else {
+            sec = newsec % 60;
+            min += 1;
+            if (min > 60) {
+                min = min % 60;
+                hour += 1;
+            }
+        }
+    }
+    
+    void showtime() {
+        cout << hour << ":" << min << ":" << sec << endl;
+    }
+    
+    Clock& operator ++ () {
+        sec += 1;
+        if (sec == 60) {
+            sec = 0;
+            min += 1;
+            if (min == 60) {
+                min = 0;
+                hour = (hour + 1) % 24;
+            }
+        }
+        return *this;
+    }
+    
+    Clock operator ++ (int) {
+        Clock old = *this;
+        ++(*this);
+        return old;
+    }
+    
+};
+
 int main(int argc, const char * argv[]) {
 
     Complex a = Complex(2, 5);
@@ -62,6 +120,13 @@ int main(int argc, const char * argv[]) {
     b = b - 5;
     
     b.display();
+    
+    
+    Clock clock = Clock(25, 42, 70);
+    clock.showtime();
+    
+    clock++;
+    clock.showtime();
     
     return 0;
 }
